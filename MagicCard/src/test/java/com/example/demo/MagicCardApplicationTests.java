997ad7737559c5c18f109a9e2cbe07cba8a9563b
vineelpellella <vineel.royal@gmail.com>
2022-07-28 17:52:05 +0530

    Resolved failed test cases

--- MagicCard/src/test/java/com/example/demo/MagicCardApplicationTests.java
+++ MagicCard/src/test/java/com/example/demo/MagicCardApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.infy.magiccard.MagicCardApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MagicCardApplication.class)
 class MagicCardApplicationTests {
 
 	@Test
